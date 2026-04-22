6ca2db8c8a0f3a863c83acbdbc24b0970a6bc0ad
T-a-r-i-q <tna003@student.usc.edu.au>
2026-04-22 21:11:58 +1000

    Initial 1c commit with inheritance package
    assets inheritance package imported and configuration of current assets so that they function as intended has been completed.
    
    SpawnPoint has been added to spaceship for bullet spawn reference point.
    
    bullet prefap reconfigured to use move constantly instead of linear movement for now as this caused a conflict with triple shot script.
    
    enemy bullets configured to use move constantly also
    
    enemy spaceship is trigger flag turned on.
    
    Spawner configured to spawn enemy ships and powerups at appropriate intervals.
    
    KNOWN ISSUES:
    player tag on bullets causes crash on collision with power ups as the bullet (considered player) does not have an attached player input script, should be resolvable by using a differing tag.

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,54 +1,88 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/*
-This script is responsible for taking player input and calling the appropriate functions 
-in our other scripts reducing update clutter and making our code more modular
-*/
+/// <summary>
+/// PlayerInput handles all of the player specific input behaviour, and passes the input information
+/// to the appropriate scripts.
+/// </summary>
 public class PlayerInput : MonoBehaviour
 {
-    // create references to our other scripts so we can call their functions
-    private PlayerMovementScript playerMovementScript;
-    private ShootingScript shootingScript;
-    
-    // Start is called before the first frame update, we will use this to initialize our script references
-    void Start()
+
+    // local references
+    private PlayerMovement playerMovement;
+
+    private WeaponBase weapon;
+    public WeaponBase Weapon
     {
-        playerMovementScript = GetComponent<PlayerMovementScript>();
-        shootingScript = GetComponent<ShootingScript>();
+        get
+        {
+            return weapon;
+        }
+
+        set
+        {
+            weapon = value;
+        }
     }
 
-    /*
-    Update is called once per frame, we will use this to check for player input and call 
-    the appropriate functions in our other scripts.
-    */
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        weapon = GetComponent<WeaponBase>();
+    }
+
     void Update()
     {
-        // Get the horizontal input axis (A/D keys or Left/Right arrow keys)
+        // read our horizontal input axis
         float horizontalInput = Input.GetAxis("Horizontal");
-
-        // If there is horizontal input, call the MoveHorizontal function in our PlayerMovementScript
-        if (horizontalInput != 0.0f) 
+        // if movement input is not zero
+        if (horizontalInput != 0.0f)
         {
-            playerMovementScript.MoveHorizontal(horizontalInput);
-        }   
-
-        /*
-        If the player presses the fire button (default is left ctrl or mouse left click), 
-        call the shoot function in our ShootingScript, but only if we have a reference to it! 
-        (if shootingScript is not null)
-        */
-        if (Input.GetButton("Fire1"))
-        {
-            if (shootingScript != null)
+            // ensure our playerMovementScript is populated to avoid errors
+            if (playerMovement != null)
             {
-                shootingScript.Shoot();
-            }
-            else
-            {
-                Debug.Log("ShootingScript not found on player!");
+                // pass our movement input to our playerMovementScript
+                playerMovement.MovePlayer(horizontalInput * Vector2.right);
             }
         }
+
+        // if we press the Fire1 button
+        if (Input.GetButton("Fire1"))
+        {
+            // if our shootingScript is populated
+            if (weapon != null)
+            {
+                // tell shootingScript to shoot
+                weapon.Shoot();
+            }
+        }
+    }
+
+    /// <summary>
+    /// SwapWeapon handles creating a new WeaponBase component based on the given weaponType. This
+    /// will popluate the newWeapon's controls and remove the existing weapon ready for usage.
+    /// </summary>
+    /// <param name="weaponType">The given weaponType to swap our current weapon to, this is an enum in WeaponBase.cs</param>
+    public void SwapWeapon(WeaponType weaponType)
+    {
+        // make a new weapon dependent on the weaponType
+        WeaponBase newWeapon = null;
+        switch (weaponType)
+        {
+            case WeaponType.machineGun:
+                newWeapon = gameObject.AddComponent<WeaponMachineGun>();
+                break;
+            case WeaponType.tripleShot:
+                newWeapon = gameObject.AddComponent<WeaponTripleShot>();
+                break;
+        }
+
+        // update the data of our newWeapon with that of our current weapon
+        newWeapon.UpdateWeaponControls(weapon);
+        // remove the old weapon
+        Destroy(weapon);
+        // set our current weapon to be the newWeapon
+        weapon = newWeapon;
     }
 }
